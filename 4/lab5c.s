ab05307119063e4487ab4b0529f25fc2ec58e23d
Abshar Mohammed Aslam <Absharaslam2001@gmail.com>
2021-10-05 11:41:17 +0400

    Add files via upload

--- 4/lab5c.s
+++ 4/lab5c.s
@@ -1,23 +1,67 @@
 # Sum of two arrays
 
 	.data
-myA:		.word	1, 2, 3, 4, 5
-myB:		.word	1, 2, 3, 4, 5
-myC:		.space	20
-prompt:		.asciiz	"\tC[i] = A[i] + B[i]:\n\n"
-len:		.word	5
+myA:		.space	80
+myB:		.space	80
+myC:		.space	80
+heading:		.asciiz	"\tC[i] = A[i] + B[i]:\n\n"
+inPrompt:		.asciiz	"Enter size of array (MAX: 20): "
+inputA:		.asciiz	"\nEnter element for A: "
+inputB:		.asciiz	"Enter element for B: "
 outArray:		.asciiz	"\n\tC["
 closer: 		.asciiz	"]:  "
 
 	.text
 main:
 
-	li $v0, 4
-	la $a0, prompt
+	li $v0, 4		# Heading
+	la $a0, heading
 	syscall
 
-	lw $t0, len		# Length of Array
+	li $v0, 4
+	la $a0, inPrompt	# Array Length Input
+	syscall
+
+	li $v0, 5		# Length of Array
+	syscall
+	add $t0, $zero, $v0
+
 	li $t1, 0		# Index i
+	la $s0, myA	# Base address of A
+	la $s1, myB	# Base address of B
+	la $s2, myB	# Base address of C
+
+input:	
+	bge $t1, $t0, sum
+	
+	li $v0, 4		# Prompt for A
+	la $a0, inputA
+	syscall
+
+	li $v0, 5		# Input for A
+	syscall
+
+	sw $v0, 0($s0)	# Store into A
+
+
+	li $v0, 4		# Prompt for B
+	la $a0, inputB
+	syscall
+
+	li $v0, 5		# Input for B
+	syscall
+
+	sw $v0, 0($s1)	# Store into B
+
+	addi $s0, $s0, 4
+	addi $s1, $s1, 4
+	addi $s2, $s2, 4
+	addi $t1, $t1, 1
+
+	j input
+
+sum:
+ 	li $t1, 0		# Index i
 	la $s0, myA	# Base address of A
 	la $s1, myB	# Base address of B
 	la $s2, myB	# Base address of C
