99e50a82afcd58952a8b8990e67a05beec081e99
Abshar Mohammed Aslam <Absharaslam2001@gmail.com>
2021-10-12 11:10:37 +0400

    Add Lab 5

---
+++ 5/lab5a.s
@@ -0,0 +1,76 @@
+	.data
+arrayA:		.space	40
+len:		.word	10
+enterIn:	.asciiz	"Enter 10 integer values: "
+largestPrompt:	.asciiz	"\n\tLargest: "
+smallestPrompt:	.asciiz	"\n\tSmallest: "
+
+	.text
+main:
+	li $v0, 4
+	la $a0, enterIn
+	syscall
+
+	lw $t0, len	# Length of Array
+
+	li $t1, 0	# Index
+	la $s0, arrayA
+
+input:
+	bge $t1, $t0, findLS
+
+	li $v0, 5
+	syscall
+
+	sw $v0, 0($s0)
+
+	addi $s0, $s0, 4
+	addi $t1, $t1, 1
+	j input
+
+findLS:
+	li $t1, 0	# Index i
+	la $s0, arrayA	# Base address of Array
+	lw $a1, 0($s0)	# Largest
+	lw $a2, 0($s0)	# Smallest
+
+loop:
+	bge $t1, $t0, out
+
+	lw $t2, 0($s0)
+
+	bgt $t2, $a1, setLargest
+next:	
+	blt $t2, $a2, setSmallest
+index:	
+	addi $s0, $s0, 4
+	addi $t1, $t1, 1
+	j loop
+	
+setLargest:
+	add $a1, $zero, $t2
+	j next
+
+setSmallest:
+	add $a2, $zero, $t2
+	j index
+
+out:
+	li $v0, 4		
+	la $a0, largestPrompt
+	syscall
+	
+	add $a0, $zero, $a1
+	li $v0, 1
+	syscall
+
+	li $v0, 4		
+	la $a0, smallestPrompt
+	syscall
+	
+	add $a0, $zero, $a2
+	li $v0, 1
+	syscall
+
+	li $v0, 10	# syscall 10 (Exit)
+	syscall		# System call
---
+++ 5/lab5b.s
@@ -0,0 +1,56 @@
+	.data
+arrayA:		.space	20
+len:		.word	5
+enterIn:	.asciiz	"Enter 10 integer values: "
+sumPrompt:	.asciiz	"\n\tSum: "
+
+	.text
+main:
+	li $v0, 4
+	la $a0, enterIn
+	syscall
+
+	lw $t0, len	# Length of Array
+
+	li $t1, 0	# Index
+	la $s0, arrayA
+
+input:
+	bge $t1, $t0, findSum
+
+	li $v0, 5
+	syscall
+
+	sw $v0, 0($s0)
+
+	addi $s0, $s0, 4
+	addi $t1, $t1, 1
+	j input
+
+findSum:
+	li $t1, 0	# Index i
+	la $s0, arrayA	# Base address of Array
+	li $a1, 0	# Sum
+
+loop: 
+	bge $t1, $t0, out
+
+	lw $t2, 0($s0)
+
+	add $a1, $a1, $t2
+
+	addi $s0, $s0, 4
+	addi $t1, $t1, 1
+	j loop
+
+out:
+	li $v0, 4		
+	la $a0, sumPrompt
+	syscall
+	
+	add $a0, $zero, $a1
+	li $v0, 1
+	syscall
+
+	li $v0, 10	# syscall 10 (Exit)
+	syscall		# System call
